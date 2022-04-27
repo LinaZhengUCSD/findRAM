d80c6a1544e25328880dd8c6c86ff6bc5637ea0c
Lina Zheng <32208663+LinaZhengUCSD@users.noreply.github.com>
2022-04-26 22:10:19 -0700

    delete

--- RAM.env/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/stg1/data2/lina/findRAM/RAM.env/lib"
-XML2_LIBS="-lxml2 -L/stg1/data2/lina/findRAM/RAM.env/lib -lz -L/stg1/data2/lina/findRAM/RAM.env/lib -llzma  -L/stg1/data2/lina/findRAM/RAM.env/lib -liconv -L/stg1/data2/lina/findRAM/RAM.env/lib -licui18n -licuuc -licudata -lm "
-XML2_INCLUDEDIR="-I/stg1/data2/lina/findRAM/RAM.env/include/libxml2 -I/stg1/data2/lina/findRAM/RAM.env/include"
-MODULE_VERSION="xml2-2.9.12"
-
